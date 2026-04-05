371ddec1b09e2dc78eb67387c93413664667af19
CS-Er-prakhar <cs.erprakharsharma@gmail.com>
2026-04-05 20:29:43 +0530

    phone added

--- src/controllers/register/register.controller.ts
+++ src/controllers/register/register.controller.ts
@@ -3,13 +3,13 @@ import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 
 export const registerUser = async(req:Request,res:Response)=>{
-    const {name,email,password}=req.body;
+    const {name,email,password,phone}=req.body;
    
     if(!name || !email || !password){
         return res.status(400).json({status:"error",message:"Please provide name,email and password"})
     }
     const hashedPassword=await bcrypt.hash(password,10);
-    const createdUser=await User.create({name,email,password:hashedPassword});
+    const createdUser=await User.create({name,email,password:hashedPassword,phone});
 
     if(!createdUser){
        return res.status(500).json({status:"error",message:"Error creating user"})
