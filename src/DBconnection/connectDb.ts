5d789d1dfb6a3eb4eaf3d02962f91b221dd35142
CS-Er-prakhar <cs.erprakharsharma@gmail.com>
2026-05-10 03:21:46 +0530

    re deploy

--- src/DBconnection/connectDb.ts
+++ src/DBconnection/connectDb.ts
@@ -1,38 +1,15 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI =
-  process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define MONGODB_URI"
-  );
-}
-
-// GLOBAL CACHE
-let cached = (global as any).mongoose;
-
-if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
-}
-
 const connectDb = async () => {
-  if (cached.conn) {
-    return cached.conn;
+  const uri = process.env.MONGODB_URI;
+
+  console.log("DEBUG MONGO URI:", uri); // 👈 ADD THIS
+
+  if (!uri) {
+    throw new Error("❌ MONGODB_URI is NOT defined");
   }
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(
-      MONGODB_URI
-    );
-  }
-
-  cached.conn = await cached.promise;
-
-  return cached.conn;
+  await mongoose.connect(uri);
 };
 
 export default connectDb;
