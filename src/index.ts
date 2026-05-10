b08c43fe9f7aeddb4bcac7cc2bee28337ff208e8
CS-Er-prakhar <cs.erprakharsharma@gmail.com>
2026-05-10 16:00:10 +0530

    test fix 1

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app";
-
 import connectDb from "./DBconnection/connectDb";
-const BASE_URL = process.env.BASE_URL;
 
-const port = Number(process.env.PORT) || 8000;
+// CONNECT DATABASE
+connectDb()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error(
+      "Failed to connect to MongoDB:",
+      err
+    );
+  });
 
-const server = app.listen(port, () => {
-  console.log(`Server running on ${port}`);
-});
-server.on("error", (error: NodeJS.ErrnoException) => {
-  if (error.code === "EADDRINUSE") {
-    // eslint-disable-next-line no-console
-    console.error(`Port ${port} is already in use. Use another port or stop conflicting service.`);
-    process.exit(1);
-  }
-  throw error;
-});
-
- await connectDb().then(() => {
-  // eslint-disable-next-line no-console
-  console.log("Connected to MongoDB");
-}).catch((err) => {
-  // eslint-disable-next-line no-console
-  console.error("Failed to connect to MongoDB:", err);
-
-});
+// EXPORT APP FOR VERCEL
+export default app;
