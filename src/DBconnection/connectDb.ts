fd0f875aea88baf176a71b36e567a8aeaaa0b4be
CS-Er-prakhar <cs.erprakharsharma@gmail.com>
2026-05-10 16:46:30 +0530

    db changes

--- src/DBconnection/connectDb.ts
+++ src/DBconnection/connectDb.ts
@@ -1,15 +1,30 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDb = async () => {
-  const uri = process.env.MONGODB_URI;
-
-  console.log("DEBUG MONGO URI:", uri); // 👈 ADD THIS
-
-  if (!uri) {
-    throw new Error("❌ MONGODB_URI is NOT defined");
+  if (isConnected) {
+    return;
   }
 
-  await mongoose.connect(uri);
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error("MONGODB_URI missing");
+  }
+
+  try {
+    const db = await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 30000,
+    });
+
+    isConnected = db.connections[0].readyState === 1;
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 export default connectDb;
