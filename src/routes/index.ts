b0addb9b6a16f39d650f2d4aed8f596898f8665b
CS-Er-prakhar <cs.erprakharsharma@gmail.com>
2026-05-13 00:04:46 +0530

    app.use added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Application } from "express";
 import user from "./user/User.route"
 import contact from "./contact/Contact.route"
+import schedule from "./schedule_booking/schedule.route"
 import { BaseEndpoints } from "../constants/Routes.constant";
 
 
 const appRoutes = (app:Application)=>{
   app.use(BaseEndpoints.Base,user)
   app.use(BaseEndpoints.Base,contact)
+  app.use(BaseEndpoints.Base,schedule)
 
   
 }
